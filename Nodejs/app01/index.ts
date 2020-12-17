9e5f73194c1b1e964bd47c38ea9c126691419995
alpha15479 <wichuda.ph.62@ubu.ac.th>
2020-12-17 17:54:16 +0700

    ก่อนจะส่ง

--- Nodejs/app01/index.ts
+++
@@ -1,6 +0,0 @@
-import * as request from 'request';
-request.get('https://api.exchangeratesapi.io/latest')
-    .on('response', res => {
-        console.log(`response statusCode : ${res.statusCode}`)
-        // console.log(res.headers)
-    })
